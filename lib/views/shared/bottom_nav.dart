6bb59764749fd2b4c89a1d24c7f5422fa60eb4c7
Ghulam Murtaza <ghulammurtazagopang3@gmail.com>
2023-08-29 17:47:48 +0500

    done with home screen

--- lib/views/shared/bottom_nav.dart
+++ lib/views/shared/bottom_nav.dart
@@ -13,60 +13,58 @@ class BottomBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<MainScreenNotifier>(
         builder: (context, mainScreenNotifier, child) {
-      return SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Container(
-            padding: const EdgeInsets.all(12),
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                BottomNavBarIcons(
+      return Padding(
+        padding: const EdgeInsets.all(8),
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              BottomNavBarIcons(
+                onTap: () {
+                  mainScreenNotifier.pageIndex = 0;
+                },
+                iconData: mainScreenNotifier.pageIndex == 0
+                    ? MaterialCommunityIcons.home
+                    : MaterialCommunityIcons.home_outline,
+              ),
+              BottomNavBarIcons(
                   onTap: () {
-                    mainScreenNotifier.pageIndex = 0;
+                    mainScreenNotifier.pageIndex = 1;
                   },
-                  iconData: mainScreenNotifier.pageIndex == 0
-                      ? MaterialCommunityIcons.home
-                      : MaterialCommunityIcons.home_outline,
-                ),
-                BottomNavBarIcons(
-                    onTap: () {
-                      mainScreenNotifier.pageIndex = 1;
-                    },
-                    iconData: mainScreenNotifier.pageIndex == 1
-                        ? Ionicons.search
-                        : Ionicons.search_outline
+                  iconData: mainScreenNotifier.pageIndex == 1
+                      ? Ionicons.search
+                      : Ionicons.search_outline
 
-                    // iconData: Ionicons.search,
-                    ),
-                BottomNavBarIcons(
-                    onTap: () {
-                      mainScreenNotifier.pageIndex = 2;
-                    },
-                    iconData: mainScreenNotifier.pageIndex == 2
-                        ? Ionicons.add
-                        : Ionicons.add_circle_outline),
-                BottomNavBarIcons(
-                    onTap: () {
-                      mainScreenNotifier.pageIndex = 3;
-                    },
-                    iconData: mainScreenNotifier.pageIndex == 3
-                        ? Ionicons.cart
-                        : Ionicons.cart_outline),
-                BottomNavBarIcons(
-                    onTap: () {
-                      mainScreenNotifier.pageIndex = 4;
-                    },
-                    iconData: mainScreenNotifier.pageIndex == 4
-                        ? Ionicons.person
-                        : Ionicons.person_outline),
-              ],
-            ),
+                  // iconData: Ionicons.search,
+                  ),
+              BottomNavBarIcons(
+                  onTap: () {
+                    mainScreenNotifier.pageIndex = 2;
+                  },
+                  iconData: mainScreenNotifier.pageIndex == 2
+                      ? Ionicons.add
+                      : Ionicons.add_circle_outline),
+              BottomNavBarIcons(
+                  onTap: () {
+                    mainScreenNotifier.pageIndex = 3;
+                  },
+                  iconData: mainScreenNotifier.pageIndex == 3
+                      ? Ionicons.cart
+                      : Ionicons.cart_outline),
+              BottomNavBarIcons(
+                  onTap: () {
+                    mainScreenNotifier.pageIndex = 4;
+                  },
+                  iconData: mainScreenNotifier.pageIndex == 4
+                      ? Ionicons.person
+                      : Ionicons.person_outline),
+            ],
           ),
         ),
       );
--- lib/views/ui/home_screen.dart
+++ lib/views/ui/home_screen.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:online_shop/services/helper.dart';
-import 'package:provider/provider.dart';
 import '../../models/shoes_model.dart';
 import '../shared/app_style.dart';
-import '../shared/latest_shoes.dart';
-import '../shared/product_card.dart';
+import '../shared/home_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -109,119 +106,9 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                 child: TabBarView(
                   controller: _tabController,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.405,
-                            child: FutureBuilder<List<Sneakers>>(
-                              future: _male,
-                              builder: ((context, snapshot) {
-                                if (snapshot.connectionState ==
-                                    ConnectionState.waiting) {
-                                  return const CircularProgressIndicator();
-                                } else if (snapshot.hasError) {
-                                  return Text('error ${snapshot.hasError}');
-                                } else {
-                                  final male = snapshot.data;
-                                  return ListView.builder(
-                                      physics: const BouncingScrollPhysics(),
-                                      itemCount: male!.length,
-                                      scrollDirection: Axis.horizontal,
-                                      itemBuilder: ((context, index) {
-                                        final male = snapshot.data![index];
-                                        return ChangeNotifierProvider.value(
-                                          value: male,
-                                          child: const ProductCard(),
-                                        );
-                                      }));
-                                }
-                              }),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 15.h,
-                          ),
-                          Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    'Latest Shoes',
-                                    style: appStyle(
-                                        20.h, Colors.black, FontWeight.bold, 0),
-                                  ),
-                                  Row(
-                                    children: [
-                                      Text(
-                                        'See All',
-                                        style: appStyle(20.h, Colors.black,
-                                            FontWeight.w500, 0),
-                                      ),
-                                      Icon(
-                                        AntDesign.caretright,
-                                        size: 20.h,
-                                      )
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 15.h,
-                          ),
-                          FutureBuilder<List<Sneakers>>(
-                            future: _male,
-                            builder: ((context, snapshot) {
-                              if (snapshot.connectionState ==
-                                  ConnectionState.waiting) {
-                                return const CircularProgressIndicator();
-                              } else if (snapshot.hasError) {
-                                return Text('error ${snapshot.hasError}');
-                              } else {
-                                final shoe = snapshot.data;
-                                return SizedBox(
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.12,
-                                  child: ListView.builder(
-                                      physics: const BouncingScrollPhysics(),
-                                      itemCount: shoe!.length,
-                                      scrollDirection: Axis.horizontal,
-                                      itemBuilder: ((context, index) {
-                                        final male = shoe[index];
-                                        return LatestShoes(
-                                          imageUrl: male.imageUrl[1],
-                                        );
-                                      })),
-                                );
-                              }
-                            }),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          height: MediaQuery.of(context).size.height * 0.405,
-                          color: Colors.green,
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          height: MediaQuery.of(context).size.height * 0.405,
-                          color: Colors.orange,
-                        ),
-                      ],
-                    )
+                    HomeWidget(male: _male),
+                    HomeWidget(male: _female),
+                    HomeWidget(male: _kids),
                   ],
                 ),
               )
